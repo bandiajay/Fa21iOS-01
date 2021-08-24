eabcf47062fc3a2a3666149eb21c767886495f62
Ajay Bandi <ajayhunters@gmail.com>
2021-08-24 08:46:48 -0500

    Variables Constants Print statements

---
+++ 02Practice.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+var sum = 0;
+var a = 10;
+var b = 20;
+sum = a+b
+print(sum)
+//String interpolation
+print("The answer is: \(sum)")
+//terminator
+print("The message is", terminator:"$ ")
+print(sum)
+
+//seperator
+print(1,2,3,4,8,9, separator:"***")
+
+//seperated by commas
+print(1.0, 2, 7.2)
+
+var str1 = "Ajay"
+var str2 = "Bandi"
+print(str1,str2)
+//Bandi, Ajay
+print(str2, ",", str1)
+print(str2, str1, separator:", ")
+
+//Constants (let) and variables (var)
+var p = 76
+var q = 22
+p = p-q
+print(p)
+
+let r = 20.0;
+//r = r+q illegal statement
+let PI = 3.14;
+var area = PI*r*r;
+print(area)
+print("The area of the circle is: \(area)")
+
+
+
+
